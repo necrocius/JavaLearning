27c9904018ac9781a39dcc354ebd525daef2f162
necrocius <100ishundred@gmail.com>
2016-10-14 01:35:43 +0500

    Created class example2

---
+++ src/example2.java
@@ -0,0 +1,9 @@
+class example2 {
+    public static void main(String args[]) {
+        int num;
+        num = 100;
+        System.out.println("num equals " + num);
+        num = num*2;
+        System.out.println("double num equals " + num);
+    }
+}
