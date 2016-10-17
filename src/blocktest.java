8c355595cf11f28a20f9027ecd81d94c0e52c4de
necrocius <100ishundred@gmail.com>
2016-10-17 16:22:37 +0500

    Created class blocktest

---
+++ src/blocktest.java
@@ -0,0 +1,11 @@
+class blocktest {
+    public static void main(String args[]) {
+        int x, y;
+        y = 20;
+        for (x = 0; x<10; x++) {
+            System.out.println(x);
+            System.out.println(y);
+            y = y - 2;
+        }
+    }
+}
